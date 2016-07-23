15f688b815f393e4b465b70984d8e0f5d4f75131
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-07-23 20:34:55 +0700

    Add QiscusPusherClient Class with methode to process data from Pusher

---
+++ Qiscus/Qiscus/QiscusPusherClient.swift
@@ -0,0 +1,40 @@
+//
+//  QiscusPusherClient.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 7/23/16.
+//  Copyright © 2016 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+public class QiscusPusherClient: NSObject {
+    
+    public class func processDataFromPusher(json json: JSON){
+        if json != nil {
+            
+            let notifTopicId = QiscusComment.getCommentTopicIdFromJSON(json)
+            let commentBeforeId = QiscusComment.getCommentBeforeIdFromJSON(json)
+            let commentId = QiscusComment.getCommentIdFromJSON(json)
+            let qiscusService = QiscusCommentClient.sharedInstance
+            
+            if QiscusComment.countCommentOntTopic(notifTopicId) > 0{
+                let isSaved = QiscusComment.getCommentFromJSON(json, topicId: notifTopicId, saved: true)
+                if isSaved {
+                    let newMessage = QiscusComment.getCommentById(commentId)
+                    if !QiscusComment.isValidCommentIdExist(commentBeforeId) {
+                        qiscusService.syncMessage(notifTopicId)
+                    }else{
+                        newMessage?.updateCommentIsSync(true)
+                    }
+                    if qiscusService.commentDelegate != nil{
+                        qiscusService.commentDelegate?.gotNewComment([newMessage!])
+                    }
+                }
+            }
+            
+        }
+    }
+
+}
