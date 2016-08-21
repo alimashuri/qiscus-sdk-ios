ed701cc64a172fdfd267a4830e37bdf01da3bf56
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-08-21 12:11:15 +0700

    add QiscusChatCellDelegate

---
+++ Qiscus/Qiscus/QiscusChatCellDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  QiscusChatCellDelegate.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 8/21/16.
+//  Copyright © 2016 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+class QiscusChatCellDelegate: NSObject {
+
+}
