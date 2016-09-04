9b46b69f24117e034ee895338f9621038066fc9a
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-09-04 10:18:20 +0700

    use masking image on message type image

--- Qiscus/Qiscus/ChatCellMedia.swift
+++ Qiscus/Qiscus/ChatCellMedia.swift
@@ -11,25 +11,26 @@ import QAsyncImageView
 
 public class ChatCellMedia: UITableViewCell {
 
-    @IBOutlet weak var mediaDisplay: UIImageView!
-    @IBOutlet weak var fileIcon: UIImageView!
-    @IBOutlet weak var bubleView: UIView!
-    @IBOutlet weak var leftArrow: UIImageView!
-    @IBOutlet weak var rightArrow: UIImageView!
+    @IBOutlet weak var imageDisplay: UIImageView!
+    @IBOutlet weak var displayFrame: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var statusImage: UIImageView!
-    @IBOutlet weak var fileNameLabel: UILabel!
     @IBOutlet weak var progressLabel: UILabel!
     @IBOutlet weak var downloadButton: ChatFileButton!
     @IBOutlet weak var progressContainer: UIView!
     @IBOutlet weak var progressView: UIView!
     @IBOutlet weak var dateLabelRightMargin: NSLayoutConstraint!
-    @IBOutlet weak var leftMargin: NSLayoutConstraint!
     @IBOutlet weak var progressHeight: NSLayoutConstraint!
+    @IBOutlet weak var displayLeftMargin: NSLayoutConstraint!
+    @IBOutlet weak var displayWidth: NSLayoutConstraint!
+    @IBOutlet weak var displayOverlay: UIView!
+    @IBOutlet weak var downloadButtonTrailing: NSLayoutConstraint!
+    
     
     let defaultDateLeftMargin:CGFloat = -10
     var tapRecognizer: ChatTapRecognizer?
     let maxProgressHeight:CGFloat = 36.0
+    var maskImage: UIImage?
     
     var screenWidth:CGFloat{
         get{
@@ -39,18 +40,20 @@ public class ChatCellMedia: UITableViewCell {
     
     override public func awakeFromNib() {
         super.awakeFromNib()
-        bubleView.layer.cornerRadius = 14
         statusImage.contentMode = .ScaleAspectFit
-        mediaDisplay.contentMode = .ScaleAspectFill
-        mediaDisplay.layer.cornerRadius = 10
-        mediaDisplay.userInteractionEnabled = true
-        fileIcon.image = UIImage(named: "ic_img")?.imageWithRenderingMode(.AlwaysTemplate)
-        fileIcon.tintColor = UIColor(red: 153/255.0, green: 153/255.0, blue: 153/255.0, alpha: 1)
         progressContainer.layer.cornerRadius = 20
         progressContainer.clipsToBounds = true
         progressContainer.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.65).CGColor
         progressContainer.layer.borderWidth = 2
-    
+        downloadButton.setImage(Qiscus.image(named: "ic_download_chat")!.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
+        downloadButton.tintColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)
+        self.imageDisplay.contentMode = .ScaleAspectFill
+        self.imageDisplay.clipsToBounds = true
+        self.imageDisplay.backgroundColor = UIColor.blackColor()
+        self.imageDisplay.userInteractionEnabled = true
+        self.displayFrame.contentMode = .ScaleAspectFill
+        //let topColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+        self.displayOverlay.verticalGradientColor(UIColor(red: 0, green: 0, blue: 0, alpha: 0), bottomColor: UIColor.blackColor())
     }
 
     override public func setSelected(selected: Bool, animated: Bool) {
@@ -58,43 +61,56 @@ public class ChatCellMedia: UITableViewCell {
 
     }
     
-    public func setupCell(comment:QiscusComment, first:Bool, position:CellPosition){
+    public func setupCell(comment:QiscusComment, last:Bool, position:CellPosition){
         
         let file = QiscusFile.getCommentFileWithComment(comment)
         progressContainer.hidden = true
         progressView.hidden = true
         
         if self.tapRecognizer != nil{
-            self.mediaDisplay.removeGestureRecognizer(self.tapRecognizer!)
+            self.imageDisplay.removeGestureRecognizer(self.tapRecognizer!)
             self.tapRecognizer = nil
         }
-        self.mediaDisplay.image = nil
-        leftArrow.hidden = true
-        rightArrow.hidden = true
-        leftArrow.image = Qiscus.image(named: "ic_arrow_bubble_primary")?.imageWithRenderingMode(.AlwaysTemplate)
-        rightArrow.image = Qiscus.image(named: "ic_arrow_buble_primary_light")?.imageWithRenderingMode(.AlwaysTemplate)
-        leftArrow.tintColor = QiscusUIConfiguration.sharedInstance.leftBaloonColor
-        rightArrow.tintColor = QiscusUIConfiguration.sharedInstance.rightBaloonColor
         
-        if first {
+        let thumbLocalPath = file?.fileURL.stringByReplacingOccurrencesOfString("/upload/", withString: "/upload/w_30,c_scale/")
+        
+        self.imageDisplay.image = nil
+
+        maskImage = UIImage()
+        var imagePlaceholder = Qiscus.image(named: "media_balloon")
+        if last {
+            displayWidth.constant = 147
             if position == .Left{
-                leftArrow.hidden = false
+                maskImage = Qiscus.image(named: "balloon_mask_left")!
+                imagePlaceholder = Qiscus.image(named: "media_balloon_left")
+                displayLeftMargin.constant = 4
+                downloadButtonTrailing.constant = -46
             }else{
-                rightArrow.hidden = false
+                maskImage = Qiscus.image(named: "balloon_mask_right")!
+                imagePlaceholder = Qiscus.image(named: "media_balloon_right")
+                displayLeftMargin.constant = screenWidth - 166
+                downloadButtonTrailing.constant = -61
+            }
+        }else{
+            displayWidth.constant = 132
+            maskImage = Qiscus.image(named: "balloon_mask")
+            downloadButtonTrailing.constant = -46
+            if position == .Left{
+                displayLeftMargin.constant = 19
+            }else{
+                displayLeftMargin.constant = screenWidth - 166
             }
         }
-        fileNameLabel.text = file?.fileName
+        self.displayFrame.image = maskImage
+        self.imageDisplay.image = imagePlaceholder
+        
         dateLabel.text = comment.commentTime.lowercaseString
         progressLabel.hidden = true
         if position == .Left {
-            leftMargin.constant = 15
-            bubleView.backgroundColor = QiscusUIConfiguration.sharedInstance.leftBaloonColor
             dateLabel.textColor = QiscusUIConfiguration.sharedInstance.leftBaloonTextColor
             dateLabelRightMargin.constant = defaultDateLeftMargin
             statusImage.hidden = true
         }else{
-            leftMargin.constant = screenWidth - 170
-            bubleView.backgroundColor = QiscusUIConfiguration.sharedInstance.rightBaloonColor
             dateLabel.textColor = QiscusUIConfiguration.sharedInstance.rightBaloonTextColor
             dateLabelRightMargin.constant = -28
             statusImage.hidden = false
@@ -107,7 +123,7 @@ public class ChatCellMedia: UITableViewCell {
             }else if comment.commentStatus == .Failed {
                 dateLabel.text = QiscusUIConfiguration.sharedInstance.failedText
                 dateLabel.textColor = QiscusUIConfiguration.sharedInstance.failToSendColor
-                statusImage.image = UIImage(named: "ic_warning")?.imageWithRenderingMode(.AlwaysTemplate)
+                statusImage.image = Qiscus.image(named: "ic_warning")?.imageWithRenderingMode(.AlwaysTemplate)
                 statusImage.tintColor = QiscusUIConfiguration.sharedInstance.failToSendColor
             }
             
@@ -117,6 +133,9 @@ public class ChatCellMedia: UITableViewCell {
         if file != nil {
             if !file!.isLocalFileExist() {
                 
+                print("thumbLocalPath: \(thumbLocalPath)")
+                self.imageDisplay.loadAsync(thumbLocalPath!)
+                //self.imageDispay.image = UIImageView.maskImage(Qiscus.image(named: "testImage")!, mask: Qiscus.image(named: "balloon_mask_left")!)
                 if file!.isDownloading {
                     self.downloadButton.hidden = true
                     self.progressLabel.text = "\(Int(file!.downloadProgress * 100)) %"
@@ -128,16 +147,15 @@ public class ChatCellMedia: UITableViewCell {
                     self.progressView.layoutIfNeeded()
                 }else{
                     self.downloadButton.comment = comment
-                    self.fileNameLabel.hidden = false
-                    self.fileIcon.hidden = false
+                    //self.fileNameLabel.hidden = false
+                    //self.fileIcon.hidden = false
                     self.downloadButton.addTarget(self, action: #selector(ChatCellMedia.downloadMedia(_:)), forControlEvents: .TouchUpInside)
                     self.downloadButton.hidden = false
                 }
             }else{
                 self.downloadButton.hidden = true
-                self.mediaDisplay.loadAsync("file://\(file!.fileThumbPath)")
-                self.fileNameLabel.hidden = true
-                self.fileIcon.hidden = true
+                //self.mediaDisplay.loadAsync("file://\(file!.fileThumbPath)")
+                self.imageDisplay.loadAsync("file://\(file!.fileThumbPath)")
                 if file!.isUploading{
                     self.progressContainer.hidden = false
                     self.progressView.hidden = false
@@ -147,6 +165,7 @@ public class ChatCellMedia: UITableViewCell {
                 }
             }
         }
+        //self.imageDispay.backgroundColor = UIColor.yellowColor()
     }
     
     public func downloadMedia(sender: ChatFileButton){
@@ -154,6 +173,6 @@ public class ChatCellMedia: UITableViewCell {
         let service = QiscusCommentClient.sharedInstance
         service.downloadMedia(sender.comment!)
     }
-
+    
     
 }
