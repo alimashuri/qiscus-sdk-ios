781ba073e5a46aca2f136e8454a7192432878441
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-09-09 14:21:16 +0700

    add config delegate for error and success callback on login

---
+++ Qiscus/Qiscus/QiscusConfigDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  QiscusConfigDelegate.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 9/8/16.
+//  Copyright © 2016 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+class QiscusConfigDelegate: NSObject {
+
+}
