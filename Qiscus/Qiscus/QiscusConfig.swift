5f1931ae4bfe602f226dabe90259e48496022fed
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-09-09 14:22:46 +0700

    use parameters from QiscusMe on config

--- Qiscus/Qiscus/QiscusConfig.swift
+++ Qiscus/Qiscus/QiscusConfig.swift
@@ -15,9 +15,21 @@ public class QiscusConfig: NSObject {
     public var commentPerLoad:Int = 10
     public var BASE_URL = "https://halodoc-messaging-dev.linkdokter.com/"
     public var UPLOAD_URL = ""
-    public var USER_EMAIL = "ahmad.athaullah@gmail.com"
-    public var USER_TOKEN = ""
-    public var PUSHER_KEY = "3f27dc397124364ecc0f"
+    public var USER_EMAIL:String{
+        get{
+            return QiscusMe.sharedInstance.email
+        }
+    }
+    public var USER_TOKEN:String{
+        get{
+            return QiscusMe.sharedInstance.token
+        }
+    }
+    public var PUSHER_KEY:String{
+        get{
+            return QiscusMe.sharedInstance.rtKey
+        }
+    }
     
     public var requestHeader:[String:String]? = nil
     
@@ -34,8 +46,11 @@ public class QiscusConfig: NSObject {
     public class var UPLOAD_URL:String{
         return "\(QiscusConfig.sharedInstance.BASE_URL)/upload"
     }
+    public class var LOGIN_REGISTER:String{
+        return "\(QiscusConfig.sharedInstance.BASE_URL)/login_or_register"
+    }
     public class var ROOM_REQUEST_URL:String{
-        return "\(QiscusConfig.sharedInstance.BASE_URL)/room_create_with_participant"
+        return "\(QiscusConfig.sharedInstance.BASE_URL)/get_or_create_room_with_target"
     }
     public class var LOAD_URL:String{
         let config = QiscusConfig.sharedInstance
@@ -48,4 +63,14 @@ public class QiscusConfig: NSObject {
         //return "\(config.BASE_URL)/topic_comments/"
     }
     
+    public func setUserConfig(withEmail email:String, userKey:String, rtKey:String){
+        QiscusMe.sharedInstance.email = email
+        QiscusMe.sharedInstance.userData.setObject(email, forKey: "qiscus_email")
+        
+        QiscusMe.sharedInstance.token = userKey
+        QiscusMe.sharedInstance.userData.setObject(userKey, forKey: "qiscus_token")
+        
+        QiscusMe.sharedInstance.rtKey = rtKey
+        QiscusMe.sharedInstance.userData.setObject(rtKey, forKey: "qiscus_rt_key")
+    }
 }
