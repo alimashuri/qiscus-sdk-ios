02f4d2642d935763ea77bb1a0f2b0d1151e2ad68
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-11-09 09:56:53 +0700

    add save distinctId, optionalData and optionalDataCompletion on chat method in Qiscus object

--- Qiscus/Qiscus/Qiscus.swift
+++ Qiscus/Qiscus/Qiscus.swift
@@ -179,7 +179,7 @@ open class Qiscus: NSObject {
      - parameter title: **String** text to show as chat title (Optional), Default value : "Chat".
      - parameter subtitle: **String** text to show as chat subtitle (Optional), Default value : "" (empty string).
      */
-    open class func chat(withUsers users:[String], target:UIViewController, readOnly:Bool = false, title:String = "Chat", subtitle:String = "", distinctId:String? = nil, optionalDataCompletion: @escaping (String) -> Void){
+    open class func chat(withUsers users:[String], target:UIViewController, readOnly:Bool = false, title:String = "Chat", subtitle:String = "", distinctId:String? = nil, optionalData:String?=nil,optionalDataCompletion: @escaping (String) -> Void){
         
         if !Qiscus.sharedInstance.connected {
             Qiscus.setupReachability()
@@ -194,7 +194,12 @@ open class Qiscus: NSObject {
         
         
         let chatVC = QiscusChatVC.sharedInstance
-        chatVC.distincId = distinctId
+        if distinctId != nil{
+            chatVC.distincId = distinctId!
+        }
+        chatVC.optionalData = optionalData
+        chatVC.optionalDataCompletion = optionalDataCompletion
+        
         let navController = UINavigationController()
         navController.viewControllers = [chatVC]
         
