06aa2097f9a6e8aad08cceefe8c49d08964c2cec
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-12-13 14:10:38 +0700

    add gallery item class

---
+++ Qiscus/Qiscus/QiscusGalleryItem.swift
@@ -0,0 +1,15 @@
+//
+//  QiscusGalleryItem.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 12/9/16.
+//  Copyright © 2016 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+open class QiscusGalleryItem: NSObject {
+    var image:UIImage?
+    var url:String = ""
+    var isVideo:Bool = false
+}
