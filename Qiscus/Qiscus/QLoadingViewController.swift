7dd9cf736fc9cfe164e6ad77158dbcb5df475615
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-12-16 15:03:21 +0700

    - add loading images - remove unused file

---
+++ Qiscus/Qiscus/QLoadingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QLoadingViewController.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 12/16/16.
+//  Copyright © 2016 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+class QLoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
