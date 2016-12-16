47a5f4e21089fd9f0d9b01ccfd77183cfefb8c9f
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-12-16 15:03:49 +0700

    Add loading library

--- Qiscus/Qiscus/QLoadingViewController.swift
+++ Qiscus/Qiscus/QLoadingViewController.swift
@@ -8,20 +8,67 @@
 
 import UIKit
 
-class QLoadingViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+open class QLoadingViewController: UIViewController {
+    open static let sharedInstance = QLoadingViewController()
+    
+    @IBOutlet weak var loadingImage: UIImageView!
+    @IBOutlet weak var loadingLabel: UILabel!
+    @IBOutlet weak var percentageLabel: UILabel!
+    
+    open var percentage:Float = 0
+    open var showPercentage = false
+    open var showText = false
+    open var isPresence = false
+    open var isBlocking = false
+    open var interuptLoadingAction:()->Void = ({})
+    
+    fileprivate init() {
+        super.init(nibName: "QLoadingViewController", bundle: Qiscus.bundle)
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true , animated: false)
+        self.loadingLabel.isHidden = !showText
+        self.percentageLabel.isHidden = !showText
+        
+    }
+    override open func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true , animated: false)
+        self.isPresence = true
+        var images: [UIImage] = []
+        for i in 0...11 {
+            images.append(Qiscus.image(named: "loading\(i+1)")!)
+        }
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(QLoadingViewController.onTapLoading))
+        self.view.addGestureRecognizer(tap)
+        
+        self.loadingImage.animationImages = images
+        self.loadingImage.animationDuration = 1
+        self.loadingImage.animationRepeatCount = 0
+        self.loadingImage.startAnimating()
+    }
+    override open func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.isPresence = false
+    }
+    override open func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
+    open func onTapLoading() {
+        if !self.isBlocking{
+            self.dismiss(animated: true, completion: {
+                self.interuptLoadingAction()
+            })
+        }
+    }
     /*
     // MARK: - Navigation
 
@@ -31,5 +78,5 @@ class QLoadingViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
